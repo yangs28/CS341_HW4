d1338df7dd37540d552c8b175167d7572bd8a5fd
Sean Yang <yangs28@up.edu>
2026-02-15 23:06:00 -0800

    Documentationing time lmao

--- hw3/public/javascripts/dropdownMenu.js
+++ hw3/public/javascripts/dropdownMenu.js
@@ -1,6 +1,7 @@
 /* 
- * CS 341: Homework 3
+ * CS 341: Homework 4
  * Purpose: A JavaScript file that allows the dropdown to change. Allows user to select a month from the dropdown menu and have the display change appropriately.
+ * Will also send a post to the server and update the list accordingly 
  * File type: .js file
  * Author: Sean Yang
 */    
@@ -11,6 +12,7 @@
         var selectedMonth = $(this).text();
         //Attempts to convert the selectedMonth to a text object for sending
         var monthText = selectedMonth.toLowerCase();
+        //Grabs the dropdownbutton and changes the text to the selected month
         const button = document.getElementById("dropbtn");
         $("#dropbtn").text(selectedMonth);
 
